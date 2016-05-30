5c7780f98bb8ab03ae2066a4a23e2a0b10be6bfb
ArtemKhymenko <breakerhim@ua.fm>
2016-05-30 21:47:23 +0300

    Update settings and requirements to support production server

--- readit/settings.py
+++ readit/settings.py
@@ -105,6 +105,11 @@ elif DJANGO_MODE == 'staging':
         }
     }
 
+elif DJANGO_MODE == 'production':
+    import dj_database_url
+    # Hadlles DATABASE_URL environment variable
+    DATABASES = {'default': dj_database_url.config()}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -142,6 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'readit', 'static'),
