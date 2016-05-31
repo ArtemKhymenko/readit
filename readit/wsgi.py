05ae98f3505f4c3c762321d906ad54245c444404
ArtemKhymenko <breakerhim@ua.fm>
2016-05-31 12:58:19 +0300

    Fix no module error

--- readit/wsgi.py
+++ readit/wsgi.py
@@ -14,4 +14,4 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readit.settings")
 
-application = cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
